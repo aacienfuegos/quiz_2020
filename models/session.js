192b3e1eea3480491cb552e15d2521407649c055
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-05-16 17:24:37 +0200

    create model

--- models/session.js
+++ models/session.js
@@ -17,10 +17,10 @@ module.exports = (sequelize, DataTypes) => {
             },
             data: {
                 type: DataTypes.STRING(50000)
+            },
+            randomPlayResolved: {
+                type: DataTypes.ARRAY(1000)
             }
-            // randomPlayResolved: {
-            //     type: DataTypes.ARRAY(1000)
-            // }
         }, {
             sequelize
         }
